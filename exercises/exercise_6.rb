a45c34fcab358fbf256fdb1cd4d2af9028b672d5
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-01-31 19:48:35 -0500

    connect two tables and populate employees table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Sam", last_name: "Heart", hourly_rate: 40)
+@store1.employees.create(first_name: "Jack", last_name: "Thoma", hourly_rate: 25)
+
+@store2.employees.create(first_name: "Emma", last_name: "Rose", hourly_rate: 30)
+@store2.employees.create(first_name: "Joe", last_name: "Wright", hourly_rate: 32)
+@store2.employees.create(first_name: "Anna", last_name: "Thorn", hourly_rate: 27)
